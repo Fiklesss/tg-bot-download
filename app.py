b1dd2211ab23e8c723bc75ba3646b3e060d8e6dd
Fiklesss <fiklesss@gmail.com>
2024-08-08 17:41:31 +0300

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import logging
 from flask import Flask, request
 from telegram import Update, Bot
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 import yt_dlp
 
 TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -13,7 +13,7 @@ app = Flask(__name__)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context) -> None:
     update.message.reply_text('Привет! Отправь мне ссылку на YouTube видео, и я скачаю его для тебя.')
 
 def download_video(url):
@@ -27,19 +27,19 @@ def download_video(url):
         ydl.download([url])
         return video_url
 
-def handle_message(update: Update, context: CallbackContext) -> None:
+def handle_message(update: Update, context) -> None:
     url = update.message.text
     try:
         video_path = download_video(url)
         with open(video_path, 'rb') as video:
             bot.send_video(chat_id=update.message.chat_id, video=video)
     except Exception as e:
-        update.message.reply_text('Произошла ошибка при загрузке видео.')
+        update.message.reply_text(f'Произошла ошибка при загрузке видео: {e}')
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook() -> None:
     update = Update.de_json(request.get_json(force=True), bot)
-    dispatcher.process_update(update)
+    application.update_queue.put(update)
     return 'ok'
 
 @app.route('/')
@@ -47,11 +47,10 @@ def index() -> str:
     return 'Hello, this is the Telegram bot server.'
 
 if __name__ == '__main__':
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
+    application = Application.builder().token(TOKEN).build()
 
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     # Убедитесь, что Flask-приложение прослушивает правильный порт
     port = int(os.getenv('PORT', 5000))
